7f7269a44b6e3c89458694264690abda40fc578e
minjun <hitobi1014@gmail.com>
2024-10-16 01:45:32 +0900

    test. test용 fake 객체 추가

---
+++ community_feed/src/test/java/org/fastcampus/post/domain/repository/FakeCommentRepository.java
@@ -0,0 +1,30 @@
+package org.fastcampus.post.domain.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import org.fastcampus.post.application.interfaces.CommentRepository;
+import org.fastcampus.post.domain.comment.Comment;
+
+public class FakeCommentRepository implements CommentRepository {
+
+    private final Map<Long, Comment> store = new HashMap<>();
+
+    @Override
+    public Comment save(Comment comment) {
+        if (comment.getId() != null) {
+            store.put(comment.getId(), comment);
+            return comment;
+        }
+        long id = store.size() + 1;
+        Comment newComment = new Comment(id, comment.getPost(), comment.getAuthor(),
+            comment.getContentObject());
+        store.put(id, newComment);
+        return newComment;
+    }
+
+    @Override
+    public Optional<Comment> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+}
